ef6b10e41c58c5907afe7086d9ae227e336b6a9d
Veselin Petranchev <92987874+Veselinnnnnn@users.noreply.github.com>
2024-11-03 16:50:22 +0200

    removing leftovers

--- src/main/java/com/universityproject/backendproject/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/universityproject/backendproject/config/security/JwtAuthenticationFilter.java
@@ -42,8 +42,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         String jwt = extractJwtFromHeader(authHeader);
         Long userId = extractUserIdFromJwt(jwt);
-        System.out.println("Error!");
-        System.out.println(userId);
         if (userId != null && isAuthenticationAbsent()) {
             authenticateUser(request, jwt, userId);
         }
